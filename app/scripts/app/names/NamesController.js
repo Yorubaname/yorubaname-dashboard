85d7b061837c191398cc66c89443664ac5ce4a6a
Yemi Agbetunsin <yemi.agbetunsin@gmail.com>
2016-03-02 00:24:15 +0100

    passed entry.id instead of entry

--- app/scripts/app/names/NamesController.js
+++ app/scripts/app/names/NamesController.js
@@ -323,7 +323,7 @@ dashboardappApp
             $scope.delete = function(entry){
                 // delete listed feedback entry
                 if (entry && $window.confirm('Are you sure you want to delete this feedback on '+ entry.name + '?') ) {
-                    return api.deleteFeedback(entry, function(){
+                    return api.deleteFeedback(entry.id, function(){
                         $scope.feedbacks.splice( $scope.feedbacks.indexOf(entry), 1 )
                         // $scope.count--
                     })
