fb90bd5ddc075c7331787cbe2d82369f0dc93d0e
Dadepo Aderemi <daderemi@ripe.net>
2016-04-19 18:29:36 +0200

    Resolved jshint issues in AuthService.js

--- app/scripts/app/auth/AuthService.js
+++ app/scripts/app/auth/AuthService.js
@@ -1,20 +1,22 @@
+"use strict";
 /* Authentication API Endpoint Service, Extension for API requests for Signing In, Out, and Session validation. Adapted from code base */
 
 angular.module("dashboardappApp")
-    .service('authService', ['baseService', 'usersService', '$localStorage', '$state', '$rootScope', '$timeout', 'toastr', 'ENV', function(api, usersApi, $localStorage, $state, $rootScope, $timeout, toastr, ENV){
+    .service('authService', ['baseService', 'usersService', '$localStorage', '$state', '$rootScope', '$timeout',
+        'toastr', 'ENV', function(api, usersApi, $localStorage, $state, $rootScope, $timeout, toastr, ENV){
 
-    this.getUser = function(callback) {
+    this.getUser = function() {
       return api.get("/v1/auth/user").then(function(response) {
-          return response
-      })
-    }
+          return response;
+      });
+    };
 
     // Authenticates clients.
     // authData is the base64 encoding of username and password
     // on authentication the $rootScope and $cookie is updated as necessary
     this.authenticate = function(authData) {
         // encode authData to base64 here, instead
-        token = btoa(authData.email + ":" + authData.password)
+        var token = btoa(authData.email + ":" + authData.password);
         
         return api.authenticate(token).success(function(response) {
             $localStorage.isAuthenticated = true;
@@ -28,27 +30,27 @@ angular.module("dashboardappApp")
                 // Check ROLE_ADMIN first, since it supercedes all
                 if (role === "ROLE_ADMIN") {
                     $localStorage.role = "admin";
-                    return true
+                    return true;
                 }
                 else if (role === "ROLE_PRO_LEXICOGRAPHER") {
                     $localStorage.role = "lex_pro";
-                    return true
+                    return true;
                 }
                 else if (role === "ROLE_BASIC_LEXICOGRAPHER") {
                     $localStorage.role = "lex_basic";
-                    return true
+                    return true;
                 }
-            })
+            });
             
-            $rootScope.msg = {}
+            $rootScope.msg = {};
             $timeout(function(){
-              $state.go('auth.home')
-              toastr.success( "Hey " + $localStorage.username + ", you are now successfully logged in.", 'Login Successful!')
-            }, 200)
+              $state.go('auth.home');
+              toastr.success( "Hey " + $localStorage.username + ", you are now successfully logged in.", 'Login Successful!');
+            }, 200);
         }).error(function(response) {
-            toastr.error(response.message, 'Sign In Error')
-        })
-    }
+            toastr.error(response.message, 'Sign In Error');
+        });
+    };
 
     // Log out function
     this.logout = function(){
@@ -61,9 +63,9 @@ angular.module("dashboardappApp")
       delete $localStorage.baseUrl;
       $rootScope.logged_in_user = null;
       $timeout(function(){
-        $state.go('login')
-        toastr.info('You are now logged out.')
-      }, 200)
-    }
+        $state.go('login');
+        toastr.info('You are now logged out.');
+      }, 200);
+    };
 
-}])
+}]);
