e7d1cea4555abd5ce11150f52ed5272eb2ab6a66
Yemi Agbetunsin <yemi.agbetunsin@gmail.com>
2016-09-13 15:56:27 +0100

    reindented; reformated for readability

--- app/scripts/app/auth/AuthState.js
+++ app/scripts/app/auth/AuthState.js
@@ -4,30 +4,39 @@ angular.module('AuthModule').config([
   '$stateProvider',
   function ($stateProvider) {
     // State Configurations
-    $stateProvider  // Login Page
-.state('login', {
-      page_title: 'Yoruba Names - Admin - Login',
-      url: '/login',
-      templateUrl: 'tmpls/login.html',
-      controller: 'AuthController',
-      data: { requiresLogout: true }
-    })  // Authenticated
-.state('auth', {
-      abstract: true,
-      // this state url
-      url: '',
-      templateUrl: 'tmpls/authenticated.html',
-      data: { requiresLogin: true }
-    })  // Dashboard
-.state('auth.home', {
-      // this state page title
-      page_title: 'Yoruba Names - Admin - Dashboard',
-      // this state url
-      url: '/',
-      templateUrl: 'tmpls/dashboard.html',
-      ncyBreadcrumb: { label: 'Home' },
-      controller: 'NameCounterController',
-      data: { requiresBasicPriviledge: true }
-    });
+    $stateProvider
+
+      // Login Page
+      .state('login', {
+        page_title: 'Yoruba Names - Admin - Login',
+        url: '/login',
+        templateUrl: 'tmpls/login.html',
+        controller: 'AuthController',
+        data: {
+          requiresLogout: true
+        }
+      })
+
+      // Authenticated
+      .state('auth', {
+        abstract: true,
+        url: '',
+        templateUrl: 'tmpls/authenticated.html',
+        data: {
+          requiresLogin: true
+        }
+      })
+
+      // Dashboard
+      .state('auth.home', {
+        page_title: 'Yoruba Names - Admin - Dashboard',
+        url: '/',
+        templateUrl: 'tmpls/dashboard.html',
+        ncyBreadcrumb: {label: 'Home'},
+        controller: 'NameCounterController',
+        data: {
+          requiresBasicPriviledge: true
+        }
+      })
   }
-]);
+])
--- app/scripts/app/names/NamesState.js
+++ app/scripts/app/names/NamesState.js
@@ -5,47 +5,63 @@ angular.module('NamesModule').config([
   '$stateProvider',
   function ($stateProvider) {
     // State Configurations
-    $stateProvider  // Names (parent state)
-.state('auth.names', {
-      // With abstract set to true, that means this state can not be explicitly activated.
-      abstract: true,
-      url: '/names',
-      template: '<div ui-view autoscroll="false" class="mainView-animate"></div>',
-      data: { requiresBasicPriviledge: true }
-    })  // Names > New Entries
-.state('auth.names.add_entries', {
-      page_title: 'Yoruba Names - Admin - Add Name Entries',
-      ncyBreadcrumb: { label: 'Add Name Entries' },
-      url: '/new',
-      templateUrl: 'tmpls/names/new.html',
-      controller: 'NamesAddEntriesCtrl'
-    })  // edit Name Entry
-.state('auth.names.edit_entries', {
-      page_title: 'Yoruba Names - Admin - Edit Entry',
-      ncyBreadcrumb: { label: 'Edit Entry' },
-      url: '/edit/:entry',
-      templateUrl: 'tmpls/names/edit.html',
-      controller: 'namesEditEntryCtrl'
-    })  // Names > Published Names
-.state('auth.names.list_entries', {
-      page_title: 'Yoruba Names - Admin - Names',
-      ncyBreadcrumb: { label: 'Names Entries' },
-      url: '/lists/:status?:submmittedBy',
-      templateUrl: 'tmpls/names/lists.html',
-      controller: 'namesListEntriesCtrl'
-    })  // Names > Name Search
-.state('auth.names.search', {
-      page_title: 'Yoruba Names - Admin - Name Search',
-      ncyBreadcrumb: { label: 'Search' },
-      url: '/search/:entry',
-      templateUrl: 'tmpls/names/search.html',
-      controller: 'nameSearchCtrl'
-    }).state('auth.names.feedbacks', {
-      page_title: 'Yoruba Names - Admin - Feedbacks',
-      ncyBreadcrumb: { label: 'Names Feedbacks' },
-      url: '/feedbacks',
-      templateUrl: 'tmpls/names/feedbacks-list.html',
-      controller: 'namesFeedbacksCtrl'
-    });
+    $stateProvider
+
+    // Names (parent state)
+      .state('auth.names', {
+        abstract: true,
+        url: '/names',
+        template: '<div ui-view autoscroll="false" class="mainView-animate"></div>',
+        data: {
+          requiresBasicPriviledge: true
+        }
+      })
+
+    // Names > New Entries
+      .state('auth.names.add_entries', {
+        page_title: 'Yoruba Names - Admin - Add Name Entries',
+        ncyBreadcrumb: { label: 'Add Name Entries' },
+        url: '/new',
+        templateUrl: 'tmpls/names/new.html',
+        controller: 'NamesAddEntriesCtrl'
+      })
+
+    // Edit Name Entry
+      .state('auth.names.edit_entries', {
+        page_title: 'Yoruba Names - Admin - Edit Entry',
+        ncyBreadcrumb: { label: 'Edit Entry' },
+        url: '/edit/:entry',
+        templateUrl: 'tmpls/names/edit.html',
+        controller: 'namesEditEntryCtrl'
+      })
+
+    // Names > Published Names
+      .state('auth.names.list_entries', {
+        page_title: 'Yoruba Names - Admin - Names',
+        ncyBreadcrumb: { label: 'Names Entries' },
+        url: '/lists/:status?:submmittedBy',
+        templateUrl: 'tmpls/names/lists.html',
+        controller: 'namesListEntriesCtrl'
+      })
+
+    // Names > Name Search
+      .state('auth.names.search', {
+        page_title: 'Yoruba Names - Admin - Name Search',
+        ncyBreadcrumb: { label: 'Search' },
+        url: '/search/:entry',
+        templateUrl: 'tmpls/names/search.html',
+        controller: 'nameSearchCtrl'
+      })
+
+
+    // All Names Feedback
+      .state('auth.names.feedbacks', {
+        page_title: 'Yoruba Names - Admin - Feedbacks',
+        ncyBreadcrumb: { label: 'Names Feedbacks' },
+        url: '/feedbacks',
+        templateUrl: 'tmpls/names/feedbacks-list.html',
+        controller: 'namesFeedbacksCtrl'
+      })
+
   }
-]);
+])
