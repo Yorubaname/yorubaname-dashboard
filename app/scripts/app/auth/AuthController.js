178cd4fd8364ca6cbda025b5d6f94adfa46e962c
Hafiz Adewuyi <hoadewuyi@gmail.com>
2025-02-23 18:55:50 +0200

    Customize header based on domain name.

--- app/scripts/app/auth/AuthController.js
+++ app/scripts/app/auth/AuthController.js
@@ -2,8 +2,19 @@
 /* Controllers */
 angular.module('AuthModule').controller('AuthController', [
   '$scope',
+  '$window',
   'AuthService',
-  function ($scope, authService) {
+  function ($scope, $window, authService) {
+    var domain = $window.location.hostname;
+
+    if (domain.includes("igboname")) {
+      $scope.siteAltText = "Igbo Names";
+      $scope.topBarColor = "igbo-theme";
+    } else {
+      $scope.siteAltText = "Yoruba Names";
+      $scope.topBarColor = "yoruba-theme";
+    }
+
     $scope.login = {};
     $scope.submit = function () {
       return authService.authenticate($scope.login);
